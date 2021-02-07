6e62460d5d9a0da3bc62fbc1e296ca6d7a3cb7a9
picanum <10380748+picanum@users.noreply.github.com>
2021-02-07 15:59:37 +0100

    Update script_cat.R

--- script_cat.R
+++ script_cat.R
@@ -9,6 +9,8 @@ library(tidyverse)
 library(patchwork)
 library(ggwordcloud)
 library(factoextra)
+library(igraph)
+library(ggraph)
 
 cs <- as.character(read.table("cs.txt", sep = "\t", encoding = "UTF-8")[,1])
 
@@ -137,6 +139,22 @@ d2erc <- palabrizador(d_erc, palabras)
 d <- data.frame(palabras = d2pp[,1], pp = d2pp[,2], psoe = d2psoe[,2], cup = d2cup[,2], 
                 pod = d2pod[,2], cs = d2cs[,2], junts = d2junts[,2], 
                 pdecat = d2pdecat[,2], erc = d2erc[,2])
+
+apply(d[,2:9], 2, sum, na.rm = T)
+#pp   psoe    cup    pod     cs  junts pdecat    erc 
+#1530  30015  29785  50186  15290  44378  30012  41422 
+
+data.frame(part = c("PP", "PSC", "CUP", "ECP", "Cs", "Junts", "PDeCAT", "ERC"),
+           term = c(1530, 30015, 29785, 50186, 15290, 44378, 30012, 41422)) %>%
+  mutate(part = factor(part, levels = part[order(term)])) %>%
+  ggplot(aes(x = part, y = term, label = term)) + geom_col(fill = "dodgerblue") +
+  geom_text(nudge_y = 1500, family = "Liberation Sans", size = 5) +
+  theme_minimal(base_size = 15) +
+  theme(text = element_text(family = "Liberation Sans")) +
+  labs(x = "Número de palabras no vacías", y = "Partido",
+       title = "Número de palabras no vacías en cada programa electoral de las elecciones catalanas del 14F",
+       caption = "Programas electorales accedidos a partir del listado de Betevé (https://beteve.cat/politica/programes-electorals-2021-catalunya-eleccions-14f/) | @Picanumeros")
+ggsave("numpalabras.png", dpi = 300)
 
 for(i in 2:9){
   d[which(is.na(d[,i])),i] <- 0
@@ -283,3 +301,227 @@ ggplot(puntos, aes(x = V1, y = V2, label = row.names(fit$points))) +
                                        caption = "Programas electorales accedidos a partir del listado de Betevé (https://beteve.cat/politica/programes-electorals-2021-catalunya-eleccions-14f/) | @Picanumeros")
 
 ggsave("mds.png", dpi = 300)
+
+#### N-GRAMS ####
+
+preproc_tidy <- function(texto){
+  #quitarparentesis <- function(x) gsub("\\s*\\([^\\)]+\\)","",as.character(x))
+  
+  texto2 <- preprocesamiento(texto)
+  for(i in 1:length(stopwords_cat)) texto2 <- str_replace_all(texto2, paste0(" ",stopwords_cat[i]," "), " ")
+  texto2 <- removeWords(texto2, stopwords("catalan"))
+  texto2 <- str_remove_all(texto2, "¿")
+  texto2 <- str_remove_all(texto2, "«")
+  texto2 <- str_remove_all(texto2, "»")
+  texto2 <- str_remove_all(texto2, "¦")
+  texto2 <- str_remove_all(texto2, "¡")
+  texto2 <- str_remove_all(texto2, "“")
+  texto2 <- str_remove_all(texto2, "”")
+  texto2 <- str_remove_all(texto2, "—")
+  texto2 <- str_remove_all(texto2, "-")
+  texto2 <- str_replace_all(texto2, "·l", "l")
+  texto2 <- str_remove_all(texto2, "<U+200B>")
+  texto2 <- str_remove_all(texto2, "€")
+  texto2 <- str_remove_all(texto2, "´")
+  texto2 <- str_replace_all(texto2, "’", " ")
+  texto2 <- str_remove_all(texto2, "●")
+  texto2 <- str_remove_all(texto2, "•")
+  texto2 <- str_remove_all(texto2, "–")
+
+  return(texto2)
+}
+
+pp_2gram <- preproc_tidy(pp) %>% tidy() %>% unnest_tokens(bigram, x, token = "ngrams", n = 2) %>%
+  count(bigram, sort = T)
+
+cs_2gram <- preproc_tidy(cs) %>% tidy() %>% unnest_tokens(bigram, x, token = "ngrams", n = 2) %>%
+  count(bigram, sort = T)
+
+psoe_2gram <- preproc_tidy(psoe) %>% tidy() %>% unnest_tokens(bigram, x, token = "ngrams", n = 2) %>%
+  count(bigram, sort = T)
+
+pod_2gram <- preproc_tidy(pod) %>% tidy() %>% unnest_tokens(bigram, x, token = "ngrams", n = 2) %>%
+  count(bigram, sort = T)
+
+junts_2gram <- preproc_tidy(junts) %>% tidy() %>% unnest_tokens(bigram, x, token = "ngrams", n = 2) %>%
+  count(bigram, sort = T)
+
+pdecat_2gram <- preproc_tidy(pdecat) %>% tidy() %>% unnest_tokens(bigram, x, token = "ngrams", n = 2) %>%
+  count(bigram, sort = T)
+
+erc_2gram <- preproc_tidy(erc) %>% tidy() %>% unnest_tokens(bigram, x, token = "ngrams", n = 2) %>%
+  count(bigram, sort = T)
+
+cup_2gram <- preproc_tidy(cup) %>% tidy() %>% unnest_tokens(bigram, x, token = "ngrams", n = 2) %>%
+  count(bigram, sort = T)
+
+pp_2gram %>% mutate(programa = "PP") %>%
+  bind_rows(psoe_2gram %>% filter(str_detect(bigram, "p ")==F & str_detect(bigram, " p")==F) %>% mutate(programa = "PSC")) %>%
+  bind_rows(cs_2gram %>% mutate(programa = "Ciudadanos")) %>%
+  bind_rows(pod_2gram %>% mutate(programa = "En Comú Podem")) %>%
+  bind_rows(junts_2gram %>% mutate(programa = "Junts")) %>%
+  bind_rows(pdecat_2gram %>% mutate(programa = "PDeCAT")) %>%
+  bind_rows(erc_2gram %>% filter(str_detect(bigram, "b ")==F & str_detect(bigram, " b")==F) %>% 
+              filter(str_detect(bigram, "c ")==F & str_detect(bigram, " c")==F) %>% mutate(programa = "ERC")) %>%
+  bind_rows(cup_2gram %>% mutate(programa = "CUP")) %>%
+  separate(bigram, c("word1", "word2"), sep = " ") %>%
+  filter(word1 %in% as.character(d[which(substr(palabras, 1, 7) == "científ" |
+                                           substr(palabras, 1, 7) == "cientif" |
+                                           substr(palabras, 1, 4) == "cièn"), "palabras"]) |
+           word2 %in% as.character(d[which(substr(palabras, 1, 7) == "científ" |
+                                             substr(palabras, 1, 7) == "cientif" |
+                                             substr(palabras, 1, 4) == "cièn"), "palabras"])) %>%
+  select(word1, word2, programa, n) %>% pivot_longer(-c(programa, n), names_to = "orden", values_to = "word") %>% group_by(word, programa) %>% summarise(n = sum(n)) %>%
+  filter(word %in% as.character(d[which(substr(palabras, 1, 7) == "científ" |
+                                          substr(palabras, 1, 7) == "cientif" |
+                                          substr(palabras, 1, 4) == "cièn"), "palabras"])==F) %>%
+  ggplot(., aes(x = 1, y = 1, size = n, label = word)) +
+  ggrepel::geom_text_repel(segment.size = 0, segment.alpha = 0) +
+  scale_size(range = c(3, 9), guide = FALSE) +
+  theme_bw(base_size = 16) +
+  theme(panel.grid = element_line(colour = "white", size=1),
+        axis.text = element_text(colour = "white", size = 0),
+        axis.ticks = element_line(colour = "white", size = 0)) +
+  facet_wrap(~programa) +
+  labs(title = "Wordcloud de los términos que acompañan a aquellos relacionados con la ciencia en cada programa de las elecciones catalanas del 14F",
+       x = "", y = "",
+       subtitle = "A más grande el término, más aparece acompañando a términos relacionados con la ciencia.\nNo se han filtrado por número de apariciones, por lo que los wordclouds son totalmente exhaustivos.",
+       caption = "Programas electorales accedidos a partir del listado de Betevé (https://beteve.cat/politica/programes-electorals-2021-catalunya-eleccions-14f/) | @Picanumeros")
+ggsave("words_ciencia.png", dpi = 300, width = 18, height = 10)
+
+pp_2gram %>% mutate(programa = "PP") %>%
+  bind_rows(psoe_2gram %>% filter(str_detect(bigram, "p ")==F & str_detect(bigram, " p")==F) %>% mutate(programa = "PSC")) %>%
+  bind_rows(cs_2gram %>% mutate(programa = "Ciudadanos")) %>%
+  bind_rows(pod_2gram %>% mutate(programa = "En Comú Podem")) %>%
+  bind_rows(junts_2gram %>% mutate(programa = "Junts")) %>%
+  bind_rows(pdecat_2gram %>% mutate(programa = "PDeCAT")) %>%
+  bind_rows(erc_2gram %>% filter(str_detect(bigram, "b ")==F & str_detect(bigram, " b")==F) %>% 
+              filter(str_detect(bigram, "c ")==F & str_detect(bigram, " c")==F) %>% mutate(programa = "ERC")) %>%
+  bind_rows(cup_2gram %>% mutate(programa = "CUP")) %>%
+  group_by(programa) %>% mutate(n = n/sum(n)) %>% ungroup() %>%
+  group_by(bigram) %>% mutate(media = mean(n)) %>% ungroup() %>%
+  mutate(lift = n/media) %>% arrange(desc(lift)) %>%
+  group_by(programa) %>% slice_max(lift, n = 10) %>%
+  ungroup() %>%
+  ggplot(aes(lift, fct_reorder(bigram, lift), fill = programa)) +
+  geom_col(show.legend = FALSE) +
+  theme_minimal(base_size = 16) +
+  facet_wrap(~programa, ncol = 2, scales = "free") +
+  labs(x = "Nº de veces que aparece dividido entre la media de los 8 programas", y = NULL,
+       title = "Bigramas más repetidos en el programa de cada partido\nen comparación con el resto de partidos",
+       caption = "Programas electorales accedidos a partir del listado de Betevé\n(https://beteve.cat/politica/programes-electorals-2021-catalunya-eleccions-14f/) | @Picanumeros")
+  # bind_tf_idf(bigram, programa, n) %>%
+  # arrange(desc(tf_idf))
+ggsave("bigramas.png", dpi = 300, width = 10, height = 12)
+
+gg1 <- pp_2gram %>% separate(bigram, c("word1", "word2"), sep = " ") %>% 
+  filter(n >= quantile(pp_2gram$n, 0.995)) %>% graph_from_data_frame() %>%
+  ggraph(layout = "fr") + 
+  geom_edge_link(aes(edge_alpha = log(n)), 
+                 show.legend = FALSE,
+                 arrow = arrow(type = "closed", length = unit(.15, "inches")), 
+                 end_cap = circle(.07, 'inches')) +
+  geom_node_point(color = "dodgerblue", size = 5) +
+  geom_node_text(aes(label = name), vjust = 1, hjust = 1, size = 3, col = "red") + theme_void(base_size = 16) +
+  labs(title = "Grafos de co-ocurrencia de términos en el programa electoral del PP para las elecciones catalanas del 14F",
+       subtitle = "Obtenido con el 0.5% de bigramas que más aparecen",
+       caption = "@Picanumeros")
+ggsave("g_pp.png", dpi = 300, width = 15, height = 11)
+gg2 <- psoe_2gram %>% separate(bigram, c("word1", "word2"), sep = " ") %>% 
+  filter(word1 != "p" & word2 != "p") %>%
+  filter(n >= quantile(psoe_2gram$n, 0.995)) %>% graph_from_data_frame() %>%
+  ggraph(layout = "fr") + 
+  geom_edge_link(aes(edge_alpha = log(n)), 
+                 show.legend = FALSE,
+                 arrow = arrow(type = "closed", length = unit(.15, "inches")), 
+                 end_cap = circle(.07, 'inches')) +
+  geom_node_point(color = "dodgerblue", size = 5) +
+  geom_node_text(aes(label = name), vjust = 1, hjust = 1, size = 3, col = "red") + theme_void(base_size = 16) +
+  labs(title = "Grafos de co-ocurrencia de términos en el programa electoral del PSC para las elecciones catalanas del 14F",
+       subtitle = "Obtenido con el 0.5% de bigramas que más aparecen",
+       caption = "@Picanumeros")
+ggsave("g_psc.png", dpi = 300, width = 15, height = 11)
+gg3 <- cs_2gram %>% separate(bigram, c("word1", "word2"), sep = " ") %>% 
+  filter(n >= quantile(cs_2gram$n, 0.995)) %>% graph_from_data_frame() %>%
+  ggraph(layout = "fr") + 
+  geom_edge_link(aes(edge_alpha = log(n)), 
+                 show.legend = FALSE,
+                 arrow = arrow(type = "closed", length = unit(.15, "inches")), 
+                 end_cap = circle(.07, 'inches')) +
+  geom_node_point(color = "dodgerblue", size = 5) +
+  geom_node_text(aes(label = name), vjust = 1, hjust = 1, size = 3, col = "red") + theme_void(base_size = 16) +
+  labs(title = "Grafos de co-ocurrencia de términos en el programa electoral de Ciudadanos para las elecciones catalanas del 14F",
+       subtitle = "Obtenido con el 0.5% de bigramas que más aparecen",
+       caption = "@Picanumeros")
+ggsave("g_cs.png", dpi = 300, width = 15, height = 11)
+
+gg4 <- pod_2gram %>% separate(bigram, c("word1", "word2"), sep = " ") %>% 
+  filter(n >= quantile(pod_2gram$n, 0.995)) %>% graph_from_data_frame() %>%
+  ggraph(layout = "fr") + 
+  geom_edge_link(aes(edge_alpha = log(n)), 
+                 show.legend = FALSE,
+                 arrow = arrow(type = "closed", length = unit(.15, "inches")), 
+                 end_cap = circle(.07, 'inches')) +
+  geom_node_point(color = "dodgerblue", size = 5) +
+  geom_node_text(aes(label = name), vjust = 1, hjust = 1, size = 3, col = "red") + theme_void(base_size = 16) +
+  labs(title = "Grafos de co-ocurrencia de términos en el programa electoral de En Comú Podem para las elecciones catalanas del 14F",
+       subtitle = "Obtenido con el 0.5% de bigramas que más aparecen",
+       caption = "@Picanumeros")
+ggsave("g_pod.png", dpi = 300, width = 15, height = 11)
+
+gg5 <- junts_2gram %>% separate(bigram, c("word1", "word2"), sep = " ") %>% 
+  filter(n >= quantile(junts_2gram$n, 0.995)) %>% graph_from_data_frame() %>%
+  ggraph(layout = "fr") + 
+  geom_edge_link(aes(edge_alpha = log(n)), 
+    show.legend = FALSE,
+    arrow = arrow(type = "closed", length = unit(.15, "inches")), 
+    end_cap = circle(.07, 'inches')) +
+  geom_node_point(color = "dodgerblue", size = 5) +
+  geom_node_text(aes(label = name), vjust = 1, hjust = 1, size = 3, col = "red") + theme_void(base_size = 16) +
+  labs(title = "Grafos de co-ocurrencia de términos en el programa electoral de Junts para las elecciones catalanas del 14F",
+       subtitle = "Obtenido con el 0.5% de bigramas que más aparecen",
+       caption = "@Picanumeros")
+ggsave("g_junts.png", dpi = 300, width = 15, height = 11)
+
+gg6 <- pdecat_2gram %>% separate(bigram, c("word1", "word2"), sep = " ") %>% 
+  filter(n >= quantile(pdecat_2gram$n, 0.995)) %>% graph_from_data_frame() %>%
+  ggraph(layout = "fr") + 
+  geom_edge_link(aes(edge_alpha = log(n)), 
+                 show.legend = FALSE,
+                 arrow = arrow(type = "closed", length = unit(.15, "inches")), 
+                 end_cap = circle(.07, 'inches')) +
+  geom_node_point(color = "dodgerblue", size = 5) +
+  geom_node_text(aes(label = name), vjust = 1, hjust = 1, size = 3, col = "red") + theme_void(base_size = 16) +
+  labs(title = "Grafos de co-ocurrencia de términos en el programa electoral del PDeCAT para las elecciones catalanas del 14F",
+       subtitle = "Obtenido con el 0.5% de bigramas que más aparecen",
+       caption = "@Picanumeros")
+ggsave("g_pdecat.png", dpi = 300, width = 15, height = 11)
+
+gg7 <- erc_2gram %>% separate(bigram, c("word1", "word2"), sep = " ") %>% 
+  filter(word1 != "b" & word2 != "b" & word1 != "c" & word2 != "c") %>%
+  filter(n >= quantile(erc_2gram$n, 0.995)) %>% graph_from_data_frame() %>%
+  ggraph(layout = "fr") + 
+  geom_edge_link(aes(edge_alpha = log(n)), 
+                 show.legend = FALSE,
+                 arrow = arrow(type = "closed", length = unit(.15, "inches")), 
+                 end_cap = circle(.07, 'inches')) +
+  geom_node_point(color = "dodgerblue", size = 5) +
+  geom_node_text(aes(label = name), vjust = 1, hjust = 1, size = 3, col = "red") + theme_void(base_size = 16) +
+  labs(title = "Grafos de co-ocurrencia de términos en el programa electoral de ERC para las elecciones catalanas del 14F",
+       subtitle = "Obtenido con el 0.5% de bigramas que más aparecen",
+       caption = "@Picanumeros")
+ggsave("g_erc.png", dpi = 300, width = 15, height = 11)
+
+gg8 <- cup_2gram %>% separate(bigram, c("word1", "word2"), sep = " ") %>% 
+  filter(n >= quantile(cup_2gram$n, 0.995)) %>% graph_from_data_frame() %>%
+  ggraph(layout = "fr") + 
+  geom_edge_link(aes(edge_alpha = log(n)), 
+                 show.legend = FALSE,
+                 arrow = arrow(type = "closed", length = unit(.15, "inches")), 
+                 end_cap = circle(.07, 'inches')) +
+  geom_node_point(color = "dodgerblue", size = 5) +
+  geom_node_text(aes(label = name), vjust = 1, hjust = 1, size = 3, col = "red") + theme_void(base_size = 16) +
+  labs(title = "Grafos de co-ocurrencia de términos en el programa electoral de la CUP para las elecciones catalanas del 14F",
+       subtitle = "Obtenido con el 0.5% de bigramas que más aparecen",
+       caption = "@Picanumeros")
+ggsave("g_cup.png", dpi = 300, width = 15, height = 11)
